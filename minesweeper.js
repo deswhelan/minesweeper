e17ae6ee96e1ac1785b593a53e1618f07b14d570
Des Whelan <wheland6@tcd.ie>
2019-02-23 18:43:40 +1300

    add playAgainPrompt after wins or losses

--- minesweeper.js
+++ minesweeper.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', startGame)
 // Define your `board` object here!
 var board = {
   // Choose number rows and columns here
-  cells: makeCells(4)
+  cells: makeCells(5)
 }
 
 // Create cells array
@@ -82,17 +82,19 @@ function checkForWin () {
   var cellCompletedCounter = 0;
   if (checkAllCellsAreCompleted(cellCompletedCounter) == board.cells.length) {
     lib.displayMessage('You win!')
+    playAgainPrompt()
   }
 }
 
-// Define this function to count the number of mines around the cell
-// (there could be as many as 8). You don't have to get the surrounding
-// cells yourself! Just use `lib.getSurroundingCells`: 
-//
-//   var surrounding = lib.getSurroundingCells(cell.row, cell.col)
-//
-// It will return cell objects in an array. You should loop through 
-// them, counting the number of times `cell.isMine` is true.
+function playAgainPrompt () {
+  setTimeout(function () {
+    if (confirm("Play again?")) {
+      location.reload()
+    }
+  }, 750)
+}
+
+
 function countSurroundingMines (cell) {
   var surroundingCells = lib.getSurroundingCells(cell.row, cell.col);
   var mineCounter = 0;
